f5038d43fc75644c5476d33e95ad8f84ff938e18
Pedro Campos <pak.97@hotmail.com>
2021-04-24 22:20:14 +0100

    basic bright ajustment - increase all RGB components

--- template_lab7/main/lab.js
+++ template_lab7/main/lab.js
@@ -6,6 +6,9 @@ let original,
 
 let trainTutorial;
 
+let brightRate = 1;
+let brightChange=false;
+
 /****************************
 Core funtions
 ****************************/
@@ -25,8 +28,13 @@ function setup() {
 }
 
 function draw() {
-    resizeCanvas(original.width, original.height);
-    image(original, 0, 0, original.width, original.height);
+    if(brightChange){
+        resizeCanvas(copy.width, copy.height);
+        image(copy, 0, 0, copy.width, copy.height);
+    }else{
+        resizeCanvas(original.width, original.height);
+        image(original, 0, 0, original.width, original.height);
+    }
 }
 
 function keyPressed() {
@@ -38,16 +46,59 @@ function keyPressed() {
         
     }else if (keyCode === UP_ARROW) {
         console.log("up");
+        increaseBright();
+        brightChange = true;
         
     }else if (keyCode === DOWN_ARROW) {
         console.log("down");
-        
+        decreaseBright();
+        brightChange = true;
     }
 }
 
 /****************************
 Your funtions
 ****************************/
+
+function increaseBright() {
+    console.log("estou no increaseBright");
+
+    if(brightRate<3){
+        brightRate+=0.2;
+    }
+    copy = createImage(original.width, original.height);
+
+    copy.copy(original, 0, 0, original.width, original.height, 0, 0, original.width, original.height);
+
+    copy.loadPixels();
+
+    for (let i = 0; i < 4 * (copy.width * copy.height); i += 4) {
+        copy.pixels[i] = original.pixels[i] * brightRate; // r
+        copy.pixels[i + 1] = original.pixels[i + 1] * brightRate;; // g
+        copy.pixels[i + 2] = original.pixels[i + 2] * brightRate;; // b
+    }copy.updatePixels();
+    console.log("brightRate:" + brightRate);
+}
+
+function decreaseBright() {
+    console.log("estou no increaseBright");
+
+    if(brightRate>0){
+        brightRate-=0.2;
+    }
+    copy = createImage(original.width, original.height);
+
+    copy.copy(original, 0, 0, original.width, original.height, 0, 0, original.width, original.height);
+
+    copy.loadPixels();
+
+    for (let i = 0; i < 4 * (copy.width * copy.height); i += 4) {
+        copy.pixels[i] = original.pixels[i] * brightRate; // r
+        copy.pixels[i + 1] = original.pixels[i + 1] * brightRate;; // g
+        copy.pixels[i + 2] = original.pixels[i + 2] * brightRate;; // b
+    }copy.updatePixels();
+    console.log("brightRate:" + brightRate);
+}
 
 
 /****************************
@@ -79,5 +130,14 @@ function addText(){
 }
 
 function addContent(container, array){
+    var ul = document.createElement('ul');
 
+    ul.setAttribute('style', 'list-style-type: disc;');
+
+    for (i = 0; i <= array.length - 1; i ++) {
+        var li = document.createElement('li'); // create li element.
+        li.innerHTML = array[i]; // assigning text to li using array value.
+
+        ul.appendChild(li); // append li to ul.
+    }container.appendChild(ul); // add list to the div.
 }
