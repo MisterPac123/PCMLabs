fa7af8ca8b2ccc5c9605725545eb8cc4224f3a84
Pedro Leitão <48485963+pedroleitao1999@users.noreply.github.com>
2021-04-25 22:09:02 +0100

    globalThresholding

--- template_lab7/main/lab.js
+++ template_lab7/main/lab.js
@@ -10,6 +10,9 @@ let contrastRate = 1;
 let brightIncrement = 0;
 let originalChange = false;
 let negativeEfect = false;
+let globalThresholding = false;
+
+let threshold = 150;
 
 /****************************
 Core funtions
@@ -35,6 +38,9 @@ function draw() {
         image(copy, 0, 0, copy.width, copy.height);
     }else if(negativeEfect) {
         resizeCanvas(copy.width, copy.height);
+        image(copy, 0, 0, copy.width, copy.height);
+	}else if (globalThresholding) {
+        onlyGlobalThresholding();
         image(copy, 0, 0, copy.width, copy.height);
     }else{
         resizeCanvas(original.width, original.height);
@@ -78,7 +84,19 @@ function keyPressed() {
     }else if (key == 'n') {
         negativeImage();
         negativeEfect = (negativeEfect) ? false : true;
-    }
+		
+    }else if (key == 't') {
+        onlyGlobalThresholding();
+        globalThresholding = (globalThresholding) ? false : true;
+
+    }else if (key == 'a') {
+        if(globalThresholding)
+            threshold = threshold - 1;
+
+    }else if (key == 's') {
+        if(globalThresholding)
+            threshold = threshold + 1;
+    }  
 }
 
 /****************************
@@ -140,6 +158,28 @@ function negativeImage() {
     console.log("contrast change:" + contrastRate);
 }
 
+function onlyGlobalThresholding() {
+    resizeCanvas(original.width, original.height);
+
+    copy = createImage(original.width, original.height);
+
+    copy.copy(original, 0, 0, original.width, original.height, 0, 0, original.width, original.height);
+
+    copy.loadPixels();
+
+    for (let i = 0; i < 4 * (copy.width * copy.height); i += 4) {
+        if(0.30*original.pixels[i] + 0.59*original.pixels[i+1] + 0.11*original.pixels[i+2] < threshold) {
+            copy.pixels[i] = 0;
+            copy.pixels[i+1] = 0;
+            copy.pixels[i+2] = 0;
+        }else{
+            copy.pixels[i] = 255;
+            copy.pixels[i+1] = 255;
+            copy.pixels[i+2] = 255;
+        }
+    }copy.updatePixels();
+}
+
 /****************************
 Utility funtions
 ****************************/
@@ -151,7 +191,8 @@ function changeInput(key){
 function addText(){
     var div = document.getElementById("instructions");
 
-    var instructions = ['UP/DOWN arrows : increase/decrease brightness', 'LEFT/RIGHT arrows : increase/decrease contrast', ' r : reset image;', 'n : negative efect'];
+    var instructions = ['UP/DOWN arrows : Increase/decrease brightness;', 'LEFT/RIGHT arrows : Increase/decrease contrast;', 'r : Reset image;', 'n : Negative efect;',
+	't : Activate/deactivate the global thresholding aplication;', 'a : Decrease threshold value;', 's : Increase threshold value;'];
 
     var p = document.createElement("p"); // create the paragraph tag
     p.innerHTML = "Instructions: ";
